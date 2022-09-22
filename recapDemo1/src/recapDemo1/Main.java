b929e6723548e05c964c3ae11ac6bb32a42a3ff7
yigit <yigityalciin@hotmail.com>
2022-09-22 04:46:22 +0300

    recap

---
+++ recapDemo1/src/recapDemo1/Main.java
@@ -0,0 +1,22 @@
+package recapDemo1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi1 = 26;
+		int sayi2 = 251;
+		int sayi3 = 27;
+		
+		int enBuyuk = sayi1;
+		
+		if(enBuyuk<sayi2) {
+			enBuyuk = sayi2;
+		}
+		
+		if(enBuyuk<sayi3) {
+			enBuyuk = sayi3;
+		}
+		System.out.println("En büyük : "+enBuyuk );
+	}
+
+}
