51650683c50a61221868f947776d942d9ed8caf7
yigit <yigityalciin@hotmail.com>
2022-09-22 04:41:36 +0300

    conditionals

---
+++ conditionals/src/conditionals/Main.java
@@ -0,0 +1,19 @@
+package conditionals;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 22;
+		if(sayi<20) {
+			System.out.println("sayi 20'den kucuktur");
+		}
+		else if (sayi==20){
+			System.out.println("sayi 20ye esittir");
+		}
+		else{
+			System.out.println("sayi 20den buyuktur");
+		}
+		
+	}
+
+}
