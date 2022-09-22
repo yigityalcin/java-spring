6d3298b8487c9447aa9123102de70c5a1aada3c6
yigit <yigityalciin@hotmail.com>
2022-09-22 04:26:13 +0300

    datatypes

---
+++ datatypes/src/datatypes/Main.java
@@ -0,0 +1,15 @@
+package datatypes;
+
+public class Main {
+
+	public static void main(String[] args) {
+		//java case sensitive
+		
+		int sayi = 12;
+		String sehir = "AnkarA";
+		byte sayi1= 12; // -128,127
+		char karakter = 'A';
+		boolean dogruMu=false;
+	}
+
+}
